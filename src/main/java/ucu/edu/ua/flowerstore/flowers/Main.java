f4c07319d70180373e0dea8ea8d8b1a66d36210c
UlianaMatvisiv <116552600+UlianaMatvisiv@users.noreply.github.com>
2023-12-23 17:53:44 +0200

    third

--- src/main/java/ucu/edu/ua/flowerstore/flowers/Main.java
+++ src/main/java/ucu/edu/ua/flowerstore/flowers/Main.java
@@ -3,11 +3,11 @@ package ucu.edu.ua.flowerstore.flowers;
 import ucu.edu.ua.flowerstore.decorator.DiscountDecorator;
 import ucu.edu.ua.flowerstore.decorator.PaperDecorator;
 public class Main {
-    private static final int sepalL = 90;
-    private static final int flowerPrice = 89;
+    private static final int SEPALLENGTH = 90;
+    private static final int PRICE = 89;
     public static void main(String[] args) {
-        Item item = new Flower(1, sepalL, FlowerColor.RED,
-        flowerPrice, FlowerType.ROSE, "this is flower");
+        Item item = new Flower(1, SEPALLENGTH, FlowerColor.RED,
+        PRICE, FlowerType.ROSE, "this is flower");
         item = new DiscountDecorator(new PaperDecorator(item));
     }
 }
